eab445ca8eaf1db7c7479584502d3506ed7368af
randomwalker2014 <randomwalker2014@gmail.com>
2019-09-06 17:12:21 -0400

    Update wsgi.py
    edits

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,12 @@ application = Flask(__name__)
 
 @application.route('/predict', methods=['GET','POST'])
 def predict():
+    
+    lr = joblib.load("./model/model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("./model/model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
     if lr:
         try:
             json_ = request.json
@@ -33,9 +39,9 @@ if __name__ == '__main__':
     except:
         port = 12345 # If you don't provide any port the port will be set to 12345
 
-    lr = joblib.load("./model/model.pkl") # Load "model.pkl"
-    print ('Model loaded')
-    model_columns = joblib.load("./model/model_columns.pkl") # Load "model_columns.pkl"
-    print ('Model columns loaded')
+    #lr = joblib.load("./model/model.pkl") # Load "model.pkl"
+    #print ('Model loaded')
+    #model_columns = joblib.load("./model/model_columns.pkl") # Load "model_columns.pkl"
+    #print ('Model columns loaded')
 
     application.run(port=port, debug=True)
